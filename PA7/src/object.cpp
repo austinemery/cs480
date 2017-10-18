9b7894ce7b31b617ba29d469fa182c293fc67ded
JacobLee <JacobLee@nevada.unr.edu>
2017-10-18 01:27:30 -0700

    added moon text

--- PA7/src/object.cpp
+++ PA7/src/object.cpp
@@ -68,20 +68,22 @@ Object::~Object()
 
 void Object::Update(unsigned int dt, objectStruct objectData)
 {
-  //cout << (float) dt * M_PI/1000 << endl;
-
-  //angle_orbit += (float) dt * M_PI/1000 ;
-  //angle_rot += (float) dt * M_PI/1000  ;
-
-  origin.x = distanceFromSun  * cos( angle_orbit );
-  origin.y = distanceFromSun  * sin( angle_orbit );
+  
+  if( orbitPeriod != 0)
+  {
+    angle_orbit += (((float) dt * M_PI/1000) / orbitPeriod) / 50 ;
+    angle_rot += (((float) dt * M_PI/1000) / rotationPeriod * 365.25) / 50 ;
+  }
+  
+  origin.x = distanceFromSun * 1000000 * cos( angle_orbit );
+  origin.y = distanceFromSun * 1000000 * sin( angle_orbit );
 
   //model = glm::scale(glm::mat4(1.0f), glm::vec3(.1, .1, .1) );
-  model = glm::scale(glm::mat4(1.0f), glm::vec3(.1, .1, .1) );
+  model = glm::scale(glm::mat4(1.0f), glm::vec3( diameter, diameter, diameter) );
 
   model = glm::translate(model, glm::vec3(origin.x, 0.0, origin.y));
                             
-  //model = glm::rotate(model, (angle_rot), glm::vec3(0.0, 1.0, 0.0));
+  model = glm::rotate(model, (angle_rot), glm::vec3(0.0, 1.0, 0.0));
   
   
 }
